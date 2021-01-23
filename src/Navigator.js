b1003444118961a6e33ad2250348346e91abfba8
Jack Norris <jacktimothynorris@gmail.com>
2021-01-22 22:43:39 -0600

    Prepped files for development

---
+++ src/Navigator.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {View, Text} from 'react-native';
+export default class Navigator extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Test</Text>
+      </View>
+    );
+  }
+}
